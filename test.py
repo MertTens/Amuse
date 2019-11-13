f64ea5dfaf64e97f91e4e77eac43ee9fa28ef79e
Aman Bhargava <amanb2000@gmail.com>
2019-11-13 05:09:31 -0500

    Added simple test script using muse2

--- test.py
+++ test.py
@@ -1,3 +1,26 @@
-import sys
+from muse2 import *
+import delegate
+import time
 
-print(sys.path)
+museAddress = '00:55:da:b5:0d:79'
+
+
+def eeg(data,trash=True):
+	global callbackTime
+	callbackTime=time.time()
+	eeg1 = data[0]
+	eeg2 = data[1]
+	eeg3 = data[2]
+	eeg4 = data[3]
+	eeg5 = data[4]
+	for i in range(12):
+		print("{0}\n".format(eeg5[i]))
+		# processEEG( eeg1[i], eeg2[i], eeg3[i], eeg4[i], eeg5[i])
+
+
+
+
+muse = Muse(callback=eeg, address=museAddress)
+
+while True:
+	time.sleep(1)
