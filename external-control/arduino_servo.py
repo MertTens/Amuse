4a7593d2572ae7d6e5909f4234772de2a4a7611d
Aman Bhargava <amanb2000@gmail.com>
2020-02-24 17:09:19 -0500

    Add external control systems for arduino

--- external-control/arduino_servo.py
+++ external-control/arduino_servo.py
@@ -4,14 +4,14 @@ import struct
 
 # Define the serial port and baud rate.
 # Ensure the 'COM#' corresponds to what was seen in the Windows Device Manager
-ser = serial.Serial('/dev/ttyACM0', 9600)
+ser = serial.Serial('/dev/cu.usbmodem14431401', 9600)
 
 def led_on_off(numDegrees):
     if numDegrees <= 0:
         numDegrees = 1
-    elif numDegrees >= 170:
-        numDegrees = 170
-
+    elif numDegrees >= 180:
+        numDegrees = 180
+    numDegrees+=1
     asBytes = bytearray(str(numDegrees), 'utf-8')
 
     print('Bytes being written are: {}'.format(asBytes))
--- external-control/serial-servo/serial-servo.ino
+++ external-control/serial-servo/serial-servo.ino
@@ -4,13 +4,14 @@ Servo myservo;
 
 int servoPos = 0;
 int incomingByte;
+int servoPin = 9;
 
 void setup() {
   // put your setup code here, to run once:
 
-  pinMode(11, OUTPUT);
+  pinMode(servoPin, OUTPUT);
   Serial.begin(9600);
-  myservo.attach(11);
+  myservo.attach(servoPin);
 }
 
 void loop() {
@@ -22,15 +23,17 @@ void loop() {
 
   incomingByte = int(Serial.parseInt());
 
-  if(incomingByte == 2){
-    myservo.write(90);
-  }
-  else if(incomingByte == 3){
-    myservo.write(45);
+//  if(incomingByte == 2){
+//    myservo.write(90);
+//  }
+//  else if(incomingByte == 3){
+//    myservo.write(45);
+//  }
+  
   if(incomingByte != 0){
     
-//    Serial.print("Writing to servo int: ");
-//    Serial.println(incomingByte);
+    Serial.print("Writing to servo int: ");
+    Serial.println(incomingByte);
     
     myservo.write(incomingByte);
   }
