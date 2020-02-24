290da3414940574856de71ed51232badb9ce5018
Aman Bhargava <amanb2000@gmail.com>
2020-02-24 17:02:24 -0500

    Added signalProcessor module for RPI4 control system

---
+++ RPI4_Muse/signalProcessor.py
@@ -0,0 +1,15 @@
+import sys
+import os
+import numpy as np
+import pandas as pd
+import serial
+import struct
+
+
+
+def process_eeg():
+    """Takes in ___, processes it, and sends it to an arduino servo"""
+    
+    #++#
+
+
